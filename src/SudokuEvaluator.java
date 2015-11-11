21e70c759f3e3df9f49dff3801a1484ea7ec5953
Divino Kenneth Bello <kenny.bello@hotmail.com>
2015-11-11 17:25:26 -0600

    SudokuEval implemented

--- src/SudokuEvaluator.java
+++ src/SudokuEvaluator.java
@@ -2,10 +2,25 @@
  * Created by Ye on 11/10/2015.
  */
 public class SudokuEvaluator {
-    int[][] grid;
+    private int[] col;
+    private int[] row;
 
-    public SudokuEvaluator(int[][] grid) {
-        this.grid = grid;
+    public SudokuEvaluator() {
+        col = new int[9];
+        row = new int[9];
+    }
+
+    private boolean checker (int[][] grid) {
+        for (int i = 0; i <= grid.length - 1; i++) {
+            for (int j = 0; j <= grid.length - 1; j++) {
+                row[grid[j][i]] += 1;
+                col[grid[i][j]] += 1;
+                if (row[grid[j][i]] > i + 1 || col[grid[i][j]] > i + 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 
