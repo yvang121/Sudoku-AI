ac62eb3b91467ecfe860c2db46f4a46bcbb1a7db
Ye Vang <yvang@macalester.edu>
2015-11-11 14:09:16 -0600

    Added some class variables, getters/setters and constructor.

--- src/BruteForce.java
+++ src/BruteForce.java
@@ -4,8 +4,34 @@ import acm.util.RandomGenerator;
  * Created by Ye on 11/9/2015.
  */
 public class BruteForce {
+    /**
+     * After each iteration, it should check what integer values are invalid to be placed in the grid.
+     * Implement a constraint-propagation checker. Checks through column/row + subgrid to identify valid
+     * integers.
+     */
     RandomGenerator randomGenerator;
-    public BruteForce(int[][] grid) {
+    int[][] grid;
+    SudokuEvaluator bruteEval;
 
+    public BruteForce(int[][] grid) {
+        this.grid = grid;
+        this.randomGenerator = new RandomGenerator();
+        this.bruteEval = new SudokuEvaluator(grid);
+    }
+
+    public RandomGenerator getRandomGenerator() {
+        return randomGenerator;
+    }
+
+    public int[][] getGrid() {
+        return grid;
+    }
+
+    public void setRandomGenerator(RandomGenerator randomGenerator) {
+        this.randomGenerator = randomGenerator;
+    }
+
+    public void setGrid(int[][] grid) {
+        this.grid = grid;
     }
 }
