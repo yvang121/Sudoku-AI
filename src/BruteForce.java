796c7980fcaece9d257a79719698a0e1e4fe3389
Ye Vang <yvang@macalester.edu>
2015-11-11 13:32:34 -0600

    Brute force baseline implementation for solving a sudoku puzzle.

---
+++ src/BruteForce.java
@@ -0,0 +1,11 @@
+import acm.util.RandomGenerator;
+
+/**
+ * Created by Ye on 11/9/2015.
+ */
+public class BruteForce {
+    RandomGenerator randomGenerator;
+    public BruteForce(int[][] grid) {
+
+    }
+}
