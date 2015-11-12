28ba05997def7c6fd01a6779cae036db427e8310
Ye Vang <yvang@macalester.edu>
2015-11-12 01:43:22 -0600

    Removed the evaluator that comes with the brute force algorithm object.

--- src/BruteForce.java
+++ src/BruteForce.java
@@ -10,22 +10,20 @@ public class BruteForce {
      * integers.
      */
     private RandomGenerator randomGenerator;
-    private SudokuEvaluator bruteEval;
 
 
     public BruteForce() {
         this.randomGenerator = new RandomGenerator();
-        this.bruteEval = new SudokuEvaluator();
     }
 
     public int[][] implement(int[][] grid){
-        for(int i=0; i<=grid.length-1; i++){
-            for(int j=0; j<=grid.length-1; j++){
+        for(int i=0; i < grid.length; i++){
+            for(int j=0; j < grid.length; j++){
                 if(grid[j][i] == 0){
-                    grid[j][i] = randomGenerator.nextInt(1,9);
+                    grid[j][i] = randomGenerator.nextInt(1, 9);
                 }
                 if(grid[i][j] == 0){
-                    grid[i][j] = randomGenerator.nextInt(1,9);
+                    grid[i][j] = randomGenerator.nextInt(1, 9);
                 }
             }
         }
