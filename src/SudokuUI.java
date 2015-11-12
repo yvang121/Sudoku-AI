e0fc6a5bbdc5bb17727a2eff3b1fe75c30a6ae84
Ye Vang <yvang@macalester.edu>
2015-11-12 02:30:53 -0600

    Unsuccessful coding of 'trial and error' brute force.

--- src/SudokuUI.java
+++ src/SudokuUI.java
@@ -1,4 +1,5 @@
 import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
 import java.util.Arrays;
 
@@ -8,17 +9,30 @@ import java.util.Arrays;
 public class SudokuUI extends GraphicsProgram {
     private SudokuGrid grid;
 
+
     /**
      * Main run method to initiate Sudoku AI program
      */
     public void run() {
-        grid = new SudokuGrid(9, "easy");
+        grid = new SudokuGrid(4, "easy");
         add(grid, 0, 0);
-        BruteForce bruteForce = new BruteForce();
-        System.out.println(Arrays.deepToString(bruteForce.implement(grid.getBackendGrid())));
-        grid.setBackendGrid(bruteForce.implement(grid.getBackendGrid()));
+//        System.out.println(Arrays.deepToString(bruteForce.implement(grid.getBackendGrid())));
+//        grid.setBackendGrid(bruteForce.implement(grid.getBackendGrid()));
         SudokuEvaluator gridCheck = new SudokuEvaluator(grid.getDimension());
-        System.out.println(gridCheck.checker(grid.getBackendGrid()));
-        grid.addNumbers(grid.getBackendGrid());
+        int[][] initGrid = grid.getBackendGrid();
+        BruteForce bruteForce = new BruteForce();
+        while (true) {
+            int[][] genBruteForce = bruteForce.implement(initGrid);
+            System.out.println(Arrays.deepToString(genBruteForce));
+            grid.addNumbers(genBruteForce);
+            if (gridCheck.checker(genBruteForce)) {
+                break;
+            } else {
+                RandomGenerator gen = new RandomGenerator();
+                bruteForce.setRandomGenerator(gen);
+            }
+        }
+//        gridCheck.checker(grid.getBackendGrid());
+//        grid.addNumbers(grid.getBackendGrid());
     }
 }
