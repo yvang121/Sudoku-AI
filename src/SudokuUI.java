f316da995ea34167f391bad6dd395b87b4e3984f
Ye Vang <yvang@macalester.edu>
2015-11-13 22:24:20 -0600

    TODO: implement drop down for difficulty.

--- src/SudokuUI.java
+++ src/SudokuUI.java
@@ -16,34 +16,35 @@ public class SudokuUI extends GraphicsProgram {
      * Main run method to initiate Sudoku AI program
      */
     public void run() {
-//        grid = new SudokuGrid(9, "M");
-//        add(grid, 0, 0);
-//        grid.addNumToUI(grid.getBackendGrid());
-//        BruteForce bruteForce = new BruteForce();
-//        System.out.println(Arrays.deepToString(grid.getBackendGrid()));
-//        System.out.println(Arrays.deepToString(bruteForce.implement(grid.getBackendGrid())));
-//        grid.setBackendGrid(bruteForce.implement(grid.getBackendGrid()));
-//        SudokuEvaluator gridCheck = new SudokuEvaluator(grid.getDimension());
+        grid = new SudokuGrid(4, "e");
+        add(grid, 0, 0);
+        grid.addNumToUI(grid.getBackendGrid());
+        BruteForce bruteForce = new BruteForce();
+        System.out.println(Arrays.deepToString(grid.getBackendGrid()));
+        System.out.println(Arrays.deepToString(bruteForce.implement(grid.getBackendGrid())));
+        grid.setBackendGrid(bruteForce.implement(grid.getBackendGrid()));
+        SudokuEvaluator gridCheck = new SudokuEvaluator(grid.getDimension());
 
-        while (true) {
-            grid = new SudokuGrid(9, "easy");
-            BruteForce bruteForce = new BruteForce();
-            int[][] genBruteForce = bruteForce.implement(grid.getBackendGrid());
-            System.out.println(Arrays.deepToString(genBruteForce));
-            SudokuEvaluator gridCheck = new SudokuEvaluator(grid.getDimension());
-            if (gridCheck.checker(genBruteForce)) {
-                add(grid, 0, 0);
-                grid.addNumToUI(genBruteForce);
-                break;
-            }
-        }
+//        while (true) {
+//            grid = new SudokuGrid(4, "easy");
+//            BruteForce bruteForce = new BruteForce();
+//            int[][] genBruteForce = bruteForce.implement(grid.getBackendGrid());
+//            System.out.println(Arrays.deepToString(genBruteForce));
+//            SudokuEvaluator gridCheck = new SudokuEvaluator(grid.getDimension());
+//            if (gridCheck.checker(genBruteForce)) {
+//                add(grid, 0, 0);
+//                grid.addNumToUI(genBruteForce);
+//                break;
+//            }
+//        }
 
-//        gridCheck.checker(grid.getBackendGrid());
-//        grid.addNumbers(grid.getBackendGrid());
+        gridCheck.checker(grid.getBackendGrid());
+        grid.addNumToUI(grid.getBackendGrid());
 
 //        petList = new JComboBox(petStrings);
 //        add(petList);
 //        petList.setSelectedIndex(4);
 //        petList.addActionListener(this);
+
     }
 }
