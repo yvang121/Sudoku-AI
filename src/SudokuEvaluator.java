985c5aea3fbf76274c2cdc071f806b92980d7d1b
Ye Vang <yvang@macalester.edu>
2015-11-14 12:02:19 -0600

    Changed Evaluator to just take a 2d grid. Telling the AI what a subgrid is is still a challenge. TODO: implement subgrid.

--- src/SudokuEvaluator.java
+++ src/SudokuEvaluator.java
@@ -4,35 +4,36 @@ import java.util.Arrays;
  * Created by Ye on 11/10/2015.
  */
 public class SudokuEvaluator {
-    private int[] col;
-    private int[] row;
-    private int[][] subgrid;
-    private int subgridSize;
-
-    public SudokuEvaluator(int gridDimension) {
-        this.col = new int[gridDimension + 1];
-        this.row = new int[gridDimension + 1];
-        this.subgridSize = (int) Math.sqrt(gridDimension);
-        this.subgrid = new int[gridDimension][gridDimension];
+    int[][] grid;
+    public SudokuEvaluator(int[][] grid) {
+        this.grid = grid;
+        this.checker();
     }
 
-    public boolean checker(int[][] grid) {
-        int[] subColArray = new int[subgridSize];
-        int[] subRowArray = new int[subgridSize];
+    public boolean checker() {
+        int dimension = grid.length;
+        int[] col = new int[dimension + 1];
+        int[] row = new int[dimension + 1];
+        int subDimension = (int) Math.sqrt(dimension);
+        int[][] subgrid = new int[dimension][dimension];
+        int[] subColArray = new int[subDimension];
+        int[] subRowArray = new int[subDimension];
 
         // Add values from grid to subgrid
-        for (int col = 0; col < grid.length; col++) {
-            for (int row = 0; row < grid.length; row++) {
-                int value = grid[col][row];
-                int subCol = (int) Math.floor(col/grid.length);
-                int subRow = (int) Math.floor(row/grid.length);
+        for (int i = 0; i < dimension; i++) {
+            int subCol = (int) Math.floor(i/subDimension);
+            for (int j = 0; j < dimension; j++) {
+                int value = grid[i][j];
+                int subRow = (int) Math.floor(j/subDimension);
+                System.out.println("Value: " + value +"\n"+ "Subgrid column: " + subCol +"\n"+ "Subgrid row: " +
+                subRow);
+                System.out.println(Arrays.deepToString(subgrid));
                 subgrid[subCol][subRow] = value;
+                System.out.println(Arrays.deepToString(subgrid));
             }
         }
-        System.out.println(Arrays.deepToString(subgrid));
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid.length; j++) {
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
                 row[grid[j][i]] += 1;
                 col[grid[i][j]] += 1;
                 if (row[grid[j][i]] > i + 1 || col[grid[i][j]] > i + 1) {
@@ -52,40 +53,6 @@ public class SudokuEvaluator {
             dashes += '-';
         }
         System.out.println(dashes + "\n" + correct + "\n" + dashes);
-        return true;
-    }
-
-    /**
-     * Generating subgrid => column / Math.sqrt(dimensions) + row - row % Math.sqrt(dimensions)
-     */
-    public boolean checkSubgrid(int[][] grid) {
-        int[] subColArray = new int[subgridSize];
-        int[] subRowArray = new int[subgridSize];
-
-        for (int col = 0; col < subgridSize; col++) {
-            for (int row = 0; row < subgridSize; row++) {
-                int value = grid[col][row];
-                int subCol = (int) Math.floor(col/grid.length);
-                int subRow = (int) Math.floor(row/grid.length);
-                subgrid[subCol][subRow] = value;
-            }
-        }
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid.length; j++) {
-                row[grid[j][i]] += 1;
-                col[grid[i][j]] += 1;
-                if (row[grid[j][i]] > i + 1 || col[grid[i][j]] > i + 1) {
-                    String incorrect = "|Solution is INCORRECT.|";
-                    String dashes = "";
-                    for (char letter : incorrect.toCharArray()) {
-                        dashes += '-';
-                    }
-                    System.out.println(dashes + "\n" + incorrect + "\n" + dashes);
-                    return false;
-                }
-            }
-        }
-        System.out.println(Arrays.deepToString(subgrid));
         return true;
     }
 }
