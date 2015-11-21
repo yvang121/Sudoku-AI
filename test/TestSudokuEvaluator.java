bd3586d88276f0716846a1f7cfd0f297c000e54b
Ye Vang <yvang@macalester.edu>
2015-11-21 03:49:41 -0600

    Deleted a line

--- test/TestSudokuEvaluator.java
+++ test/TestSudokuEvaluator.java
@@ -1,9 +1,7 @@
 import org.junit.Test;
 
-import java.util.Arrays;
-
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Ye on 11/20/2015.
@@ -14,7 +12,6 @@ public class TestSudokuEvaluator {
         int dimension = 4;
         int[][] grid = {{1,2,3,4},{3,4,1,2},{2,3,4,1},{4,1,2,3}};
         int[][] grid1 = {{2,1,4,3},{3,3,4,1},{4,3,2,1},{1,2,1,4}};
-        System.out.println(Arrays.deepToString(grid));
         SudokuEvaluator evaluator = new SudokuEvaluator(grid1);
         SudokuEvaluator evaluator1 = new SudokuEvaluator(grid);
 
