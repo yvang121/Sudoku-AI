defedd813e50f81418c1b7ceb0ae7dac60380083
Ye Vang <yvang@macalester.edu>
2015-11-21 03:01:02 -0600

    Decomposed the checker() method. Added helper methods to check only one row, or one column or one subgrid. Added helper function to check for duplicates given an array input.

--- src/SudokuEvaluator.java
+++ src/SudokuEvaluator.java
@@ -1,58 +1,146 @@
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * Created by Ye on 11/10/2015.
  */
 public class SudokuEvaluator {
     int[][] grid;
+    int[][][] subgrid;
+
     public SudokuEvaluator(int[][] grid) {
         this.grid = grid;
-        this.checker();
+        this.subgrid = generateSubgrids();
     }
 
+    /**
+     * Checks the entire grid to see if there are any duplicates in it.
+     * @return true if the checker didn't find any duplicates within the grid.
+     */
     public boolean checker() {
         int dimension = grid.length;
-        int[] col = new int[dimension + 1];
-        int[] row = new int[dimension + 1];
-        int subDimension = (int) Math.sqrt(dimension);
-        int[][] subgrid = new int[dimension][dimension];
-        int[] subColArray = new int[subDimension];
-        int[] subRowArray = new int[subDimension];
-
-        // Add values from grid to subgrid
-        for (int i = 0; i < dimension; i++) {
-            int subCol = (int) Math.floor(i/subDimension);
-            for (int j = 0; j < dimension; j++) {
-                int value = grid[i][j];
-                int subRow = (int) Math.floor(j/subDimension);
-                System.out.println("Value: " + value +"\n"+ "Subgrid column: " + subCol +"\n"+ "Subgrid row: " +
-                subRow);
-                System.out.println(Arrays.deepToString(subgrid));
-                subgrid[subCol][subRow] = value;
-                System.out.println(Arrays.deepToString(subgrid));
-            }
-        }
         for (int i = 0; i < dimension; i++) {
             for (int j = 0; j < dimension; j++) {
-                row[grid[j][i]] += 1;
-                col[grid[i][j]] += 1;
-                if (row[grid[j][i]] > i + 1 || col[grid[i][j]] > i + 1) {
-                    String incorrect = "|Solution is INCORRECT.|";
-                    String dashes = "";
-                    for (char letter : incorrect.toCharArray()) {
-                        dashes += '-';
-                    }
-                    System.out.println(dashes + "\n" + incorrect + "\n" + dashes);
+                boolean columns = checkCol(j);
+                boolean rows = checkRow(i);
+                boolean subgrids = checkSubgrid(i, j);
+                if (!(columns & rows & subgrids)) {
+                    printIncorrect();
                     return false;
                 }
             }
         }
+        printCorrect();
+        return true;
+    }
+
+    public boolean checkSubgrid(int row, int column) {
+        int subDimension = (int) Math.sqrt(grid.length);
+        int[] subArray = subgrid[(int)Math.floor(row/subDimension)][(int)Math.floor(column/subDimension)];
+        return checkDuplicates(subArray);
+    }
+
+    public boolean checkRow(int row) {
+        int[] rowArray = new int[grid.length];
+        for (int i = 0; i < grid.length; i++) {
+            if (grid[row][i] != 0) {
+                rowArray[i] = grid[row][i];
+            }
+        }
+        return checkDuplicates(rowArray);
+    }
+
+    public boolean checkCol(int col) {
+        int[] colArray = new int[grid.length];
+        for (int i = 0; i < grid.length; i++) {
+            if (grid[i][col] != 0) {
+                colArray[i] = grid[i][col];
+            }
+        }
+        return checkDuplicates(colArray);
+    }
+
+    public boolean checkDuplicates(int[] array) {
+        Set<Integer> numbers = new HashSet<Integer>();
+        for (int val : array) {
+            if (val != 0 & numbers.contains(val)) {
+                return false;
+            } else {
+                numbers.add(val);
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Checks the location for duplicates.
+     * @param row of the value
+     * @param column of the value
+     * @return true if there's no other duplicate
+     */
+    public boolean checkLocation(int row, int column) {
+        boolean columns = checkCol(column);
+        boolean rows = checkRow(row);
+        boolean subgrids = checkSubgrid(row, column);
+        if (!(columns & rows & subgrids)) {
+            return false;
+        }
+        return true;
+    }
+
+    public int[][][] generateSubgrids() {
+        int dimension = grid.length;
+        int subDimension = (int) Math.sqrt(dimension);
+        int[][][] finalSubgrid = new int[subDimension][subDimension][dimension];
+        // Add values from grid to subgrid
+        for (int i = 0; i < dimension; i++) {  // Loop through entire grid of n x n
+            for (int j = 0; j < dimension; j++) {
+                int value = grid[i][j];  // get the value
+                int subCol = (int) Math.floor(i/subDimension); // Calculate correspondence to subgrid location x
+                int subRow = (int) Math.floor(j/subDimension); // Calculate correspondence to subgrid location y
+                for (int k = 0; k < dimension; k++) {  // 3d array access
+                    if (finalSubgrid[subRow][subCol][k] == 0) {
+                        finalSubgrid[subRow][subCol][k] += value;
+                        break;
+                    }
+                }
+            }
+        }
+//        int section = -subDimension;
+//        int sectionPos = 0;
+//        for (int i=0; i < dimension; i++) {
+//            if (i%subDimension == 0){
+//                section += subDimension;
+//                sectionPos = 0;
+//            }
+//            if (i%subDimension != 0){
+//                sectionPos += subDimension;
+//            }
+//            for (int j = 0; j <dimension; j++){
+//
+//                if (j%subDimension == 0 && section != 0){
+//                    section += 1;
+//                }
+//                subgrid[section][j%subDimension + sectionPos] = grid[i][j];
+//            }
+//        }
+        return finalSubgrid;
+    }
+
+    public void printIncorrect() {
+        String incorrect = "|Solution is INCORRECT.|";
+        String dashes = "";
+        for (char letter : incorrect.toCharArray()) {
+            dashes += '-';
+        }
+        System.out.println(dashes + "\n" + incorrect + "\n" + dashes);
+    }
+
+    public void printCorrect() {
         String correct = "|Solution is CORRECT.|";
         String dashes = "";
         for (char letter : correct.toCharArray()) {
             dashes += '-';
         }
         System.out.println(dashes + "\n" + correct + "\n" + dashes);
-        return true;
     }
 }
