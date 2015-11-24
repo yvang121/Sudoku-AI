36b7b0128b35b70a0408b632abaf473c84b21e57
Ye Vang <yvang@macalester.edu>
2015-11-24 13:39:26 -0600

    Successful completion in initialization.

--- src/SudokuUI.java
+++ src/SudokuUI.java
@@ -14,16 +14,8 @@ public class SudokuUI extends GraphicsProgram {
      */
     public void run() {
         grid = new SudokuGrid(4, "e");
-        int[][] grid1 = {{1,2,3,4},{3,4,1,2},{2,3,4,1},{4,1,2,3}};
-        grid.setBackendGrids(grid1);
         add(grid, 0, 0);
         grid.addNumToUI(grid.getBackendGrid());
-//        BruteForce bruteForce = new BruteForce();
-//        grid.setBackendGrids(bruteForce.implement(grid.getBackendGrid()));
-//        SudokuEvaluator gridCheck = new SudokuEvaluator(grid.getBackendGrid());
-//        grid.addNumToUI(grid.getBackendGrid());
-//        System.out.println(Arrays.deepToString(grid.getBackendGrid()));
-//        System.out.println(Arrays.deepToString(grid.getBackendSubgrid()));
 
 //        petList = new JComboBox(petStrings);
 //        add(petList);
