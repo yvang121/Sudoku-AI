291da8553732a5f0a6ec45975e73c13088085200
Ye Vang <yvang@macalester.edu>
2015-12-02 14:41:56 -0600

    Now runs until 1,000,000 runs, and uses the same base grid without reference.

--- test/TestBruteForce.java
+++ test/TestBruteForce.java
@@ -6,35 +6,21 @@ import java.util.Arrays;
  * Created by Ye on 11/20/2015.
  */
 public class TestBruteForce {
-    SudokuGrid grid;
+    SudokuGrid grid = new SudokuGrid(4, "e");
     @Test
     public void testBruteForce() {
         int numRuns = 0;
         double before = System.currentTimeMillis();
-        while (true) {
-            grid = new SudokuGrid(4, "easy");
+        while (numRuns < 1000000) {
             BruteForce bruteForce = new BruteForce();
-            bruteForce.implement(grid.getBackendGrid());
+            int[][] tempGrid = bruteForce.implement(grid.getBackendGrid());
             System.out.println(Arrays.deepToString(grid.getBackendGrid()));
-            SudokuEvaluator gridCheck = new SudokuEvaluator(grid.getBackendGrid());
+            SudokuEvaluator gridCheck = new SudokuEvaluator(tempGrid);
             numRuns++;
             if (gridCheck.checker()) {
                 break;
             }
         }
-
-        //TODO: why does SudokuGrid also get implemented by BruteForce object?
-        // Begin trial code
-//        SudokuGrid grid = new SudokuGrid(4, "e");
-//        int[][] tempGrid = new int[4][4];
-//        System.arraycopy(grid.getBackendGrid(), 0, tempGrid, 0, tempGrid.length);
-//        System.out.println(Arrays.deepToString(tempGrid));
-//        BruteForce bruteForce = new BruteForce();
-//        bruteForce.implement(tempGrid);  // Implement only on temporary grid
-//        System.out.println(Arrays.deepToString(grid.getBackendGrid()));  // Implements on the SudokuGrid object as well
-//        System.out.println(Arrays.deepToString(tempGrid));  // Implements on the SudokuGrid object as well
-        // End trial code
-
         double after = System.currentTimeMillis();
         System.out.println("Number of total runs for Brute Force: " + numRuns +"\n"+
         "Total time taken: " + (int)((after-before)/60000) + " minutes.");
