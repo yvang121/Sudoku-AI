cd5270c8f9d5dcc68e95c018557ac7e922312106
Ye Vang <yvang@macalester.edu>
2015-12-06 03:12:10 -0600

    Implemented a literal generateNeighbors() method.

--- src/AStar.java
+++ src/AStar.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.PriorityQueue;
 
 /**
  * Created by Ye on 10/29/2015.
+ * A* heuristic search
  */
 public class AStar {
     /**
@@ -14,8 +17,22 @@ public class AStar {
 
     //TODO: specify start/goal? iterate through and find the min possible value => the obvious answers.
     public boolean AStarSearch(int[][] grid) {
+        SudokuEvaluator evaluator;
+        boolean solution = false;
         HashSet<GridNode> visited = new HashSet<GridNode>(); // coords that have been visited already + expanded
-        PriorityQueue<GridNode> fringe = new PriorityQueue<GridNode>(); // fringe coords that have yet to be visited
+        PriorityQueue<GridNode> fringe = new PriorityQueue<GridNode>(); // frontier coords that have yet to be visited
+        GridNode startNode = new GridNode(0, 0, calcHeuristic(), null);  // create start node object
+        fringe.add(startNode);  // add start node to priority queue
+        while (!solution | !fringe.isEmpty()) {  // while the solution is false or fringe set is not empty:
+            GridNode visitNode = fringe.poll();  // poll from priority queue
+            ArrayList<GridNode> neighbors = generateNeighbors(grid, visitNode);  // generate the neighbors above, left, right and below
+            // TODO: how to prevent duplicate neighbors being added into fringe set?
+            // place value into visitNode's location on backend grid based on constraints
+            // add this node into the visited hash set. should only visit every cell in grid once, hence the set
+            evaluator = new SudokuEvaluator(grid);
+            solution = evaluator.checker();
+        }
+//        if ()
         return false;
     }
 
@@ -27,5 +44,35 @@ public class AStar {
     public int calcHeuristic() {
         //TODO: implement the heuristic calculator
         return 0;
+    }
+
+    /**
+     * Generate neighbors for a given node object.
+     * @param grid backend grid to refer to for values and coordinates
+     * @param currNode the current node
+     */
+    public ArrayList<GridNode> generateNeighbors(int[][] grid, GridNode currNode) {
+        int[] neighborCells = {-1, 1};
+        // TODO: maybe we should just fill in the priority queue manually. This finds a literal path.
+        // TODO: the path could get stuck if it's surrounded by numbers != 0.
+        ArrayList<GridNode> nodes = new ArrayList<GridNode>();
+        for (int neighboringCell : neighborCells) {
+            int neighborRow = currNode.getRow() + neighboringCell;
+            int neighborCol = currNode.getColumn() + neighboringCell;
+            if (neighborRow >= 0 & neighborRow < grid.length) {
+                int neighborVal = grid[neighborRow][currNode.getColumn()];
+                if (neighborVal == 0) {
+                    GridNode newNode = new GridNode(neighborRow, currNode.getColumn(), calcHeuristic(), currNode);
+                    nodes.add(newNode);
+                }
+            }
+            if (neighborCol >= 0 & neighborRow < grid.length) {
+                int neighborVal = grid[currNode.getRow()][neighborCol];
+                if (neighborVal == 0) {
+                    GridNode newNode = new GridNode(currNode.getRow(), neighborCol, calcHeuristic(), currNode);
+                    nodes.add(newNode);
+                }
+            }
+        } return nodes;
     }
 }
