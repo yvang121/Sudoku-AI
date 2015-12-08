a0e7d4c62053e7258858c38b3b5022a75368d106
Ye Vang <yvang@macalester.edu>
2015-12-08 11:37:31 -0600

    Added comments to all methods.

--- src/Backtrack.java
+++ src/Backtrack.java
@@ -34,10 +34,23 @@ public class Backtrack {
         return false;
     }
 
+    /**
+     * Checks all columns, rows and subgrids for the input num
+     * @param row to ceck in for num
+     * @param col to check in for num
+     * @param num to check for conflicts
+     * @return true if there are no conflicts
+     */
     public boolean isOk(int row, int col, int num){
         return checkCol(col, num)&& checkRow(row, num) && checkSect(row, col, num);
     }
 
+    /**
+     * Checks a particular column for num input
+     * @param col to check in
+     * @param num integer value to check for
+     * @return true if there isn't an occurrence of num in column
+     */
     public boolean checkCol(int col, int num){
         for (int i=0; i<grid.length; i++){
             if (grid[i][col] == num){
@@ -47,6 +60,12 @@ public class Backtrack {
         return true;
     }
 
+    /**
+     * Checks a particular row for num input
+     * @param row row to check in
+     * @param num integer value to check for
+     * @return true if there isn't an occurrence of num in row
+     */
     public boolean checkRow(int row, int num){
         for(int j = 0; j<grid.length; j++){
             if (grid[row][j] == num){
@@ -56,6 +75,13 @@ public class Backtrack {
         return true;
     }
 
+    /**
+     * Checks a particular section(subgrid) for num input
+     * @param row of num
+     * @param col of num
+     * @param num integer value to check for
+     * @return true if num does not occur in section
+     */
     public boolean checkSect(int row, int col, int num){
         row = row-(row%sqRoot);
         col = col-(col%sqRoot);
@@ -70,6 +96,10 @@ public class Backtrack {
         return true;
     }
 
+    /**
+     * Finds an empty cell to place a value in.
+     * @return the coordinates of the empty cell or null if there are no empty cells.
+     */
     public int[] findEmpty(){
         int[] empty = new int[2];
         for (int i=0; i<grid.length; i++){
