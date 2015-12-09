5c4eb86b15a7d32c2b32bca2cb25c8ad2b3aba42
Ye Vang <yvang@macalester.edu>
2015-12-09 00:12:17 -0600

    Deleted block comment

--- src/BruteForce.java
+++ src/BruteForce.java
@@ -4,24 +4,7 @@ import acm.util.RandomGenerator;
  * Created by Ye on 11/9/2015.
  */
 public class BruteForce {
-    /**
-     * After each iteration, it should check what integer values are invalid to be placed in the grid.
-     * Implement a constraint-propagation checker. Checks through column/row + subgrid to identify valid
-     * integers.
-     *
-     * Backtracking:
-     *   Find row, col of an unassigned cell
-     If there is none, return true
-     For digits from 1 to 9
-     a) If there is no conflict for digit at row,col
-     assign digit to row,col and recursively try fill in rest of grid
-     b) If recursion successful, return true
-     c) Else, remove digit and try another
-     If all digits have been tried and nothing worked, return false
-     */
     private RandomGenerator randomGenerator;
-
-
     public BruteForce() {
         this.randomGenerator = new RandomGenerator();
     }
