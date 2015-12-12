4601eb03fe795d2a7e323c7439c756298245c9ec
Ye Vang <yvang@macalester.edu>
2015-12-12 14:24:42 -0600

    TODO: Finish the GUI and tying backend grid to the front end UI.

--- src/SudokuCell.java
+++ src/SudokuCell.java
@@ -5,8 +5,10 @@ import java.awt.*;
  * Created by Ye on 12/10/2015.
  */
 public class SudokuCell extends JLabel {
-    int digit; //the number it would display
-    int x, y; //the x,y position on the grid
+    private int digit; //the number it would display
+    private int x;
+    private int y; //the x,y position on the grid
+    private static final int SQUARE_DIMENSION = 40;
 
     public SudokuCell(int x, int y) {
         super();
@@ -17,8 +19,12 @@ public class SudokuCell extends JLabel {
         setBorder(BorderFactory.createLineBorder(Color.black));
 
         /** set size to 50x50 pixel for one square */
-        Dimension d = new Dimension(40,40);
+        Dimension d = new Dimension(SQUARE_DIMENSION,SQUARE_DIMENSION);
         setPreferredSize(d);
+    }
+
+    public static int getSquareDimension() {
+        return SQUARE_DIMENSION;
     }
 
     public int getDigit() {
@@ -27,6 +33,24 @@ public class SudokuCell extends JLabel {
 
     public void setDigit(int digit) {
         this.digit = digit;
+    }
+
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 }
 
